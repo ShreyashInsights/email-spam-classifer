7c96ec0eafa441c18060811d9ed300531cdf0fd6
shreyash_umbarkar <shreyashumbarkar6@gmail.com>
2024-04-02 20:27:19 +0530

    api

--- app.py
+++ app.py
@@ -17,6 +17,17 @@ def predict():
     prediction = clf.predict(tokenized_email)
     prediction = 1 if prediction == 1 else -1
     return render_template("index.html", prediction=prediction, email=email)
+# Create an API endpoi
+
+
+
+@app.route('/api/predict', methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)  # Get data posted as a json
+    email = data['content']
+    prediction = model_predict(email)
+    return jsonify({'prediction': prediction, 'email': email})  # Return prediction
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
